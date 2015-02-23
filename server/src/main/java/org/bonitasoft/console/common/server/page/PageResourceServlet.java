826c1e3ca7a1fc6cfb31d88b6c59354392863509
julien mege <julien.mege@bonitasoft.com>
2015-02-23 10:52:24 +0100

    BS-12174    As Fred, I want to call Custom Page the same way in the portal and in the living app (revert the "resources" folder deletion.

--- server/src/main/java/org/bonitasoft/console/common/server/page/PageResourceServlet.java
+++ server/src/main/java/org/bonitasoft/console/common/server/page/PageResourceServlet.java
@@ -52,7 +52,7 @@ public class PageResourceServlet extends ResourceServlet {
 
     @Override
     protected String getSubFolderName() {
-        return null;
+        return RESOURCES_SUBFOLDER_NAME;
     }
 
 }
